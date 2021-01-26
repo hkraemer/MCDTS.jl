a971fee51cb04120596186c9ef063d78ac1e936a
K. Hauke Kraemer <hkraemer@pik-potsdam.de>
2021-01-26 16:29:19 +0100

    beispiel 1 finished (8 Nodes)

--- application/artificial data/Lorenz96/analyze_Lorenz96_N8_1d.jl
+++ application/artificial data/Lorenz96/analyze_Lorenz96_N8_1d.jl
@@ -9,7 +9,8 @@ pygui(true)
 using DelimitedFiles
 
 # determine from which trial you want to process the data
-# trial 1: N=8; Fs = 3.5:0.004:5, MCDTS (80 trials), ts-length = 5000, dt = 0.1, fixed ic's
+# trial 1: N=8; Fs = 3.5:0.004:5, MCDTS (80 trials), ts-length = 5000, dt = 0.1, fixed ic's, L_thres = 0.05
+# trial 2: N=8; Fs = 3.5:0.004:5, MCDTS (80 trials), ts-length = 5000, dt = 0.1, fixed ic's, L_thres = 0
 
 trial = 1
 
@@ -53,10 +54,11 @@ L_mcdts = readdlm("./application/artificial data/Lorenz96/Results/1 dimensional 
 RQA_tde = readdlm("./application/artificial data/Lorenz96/Results/1 dimensional input/trial $(trial)/results_Lorenz96_N_8_1d_RQA_tde.csv")
 RQA_pec = readdlm("./application/artificial data/Lorenz96/Results/1 dimensional input/trial $(trial)/results_Lorenz96_N_8_1d_RQA_pec.csv")
 RQA_mcdts = readdlm("./application/artificial data/Lorenz96/Results/1 dimensional input/trial $(trial)/results_Lorenz96_N_8_1d_RQA_mcdts.csv")
+RQA_ref = readdlm("./application/artificial data/Lorenz96/Results/Reference/results_Lorenz96_N_8_ref_RQA_ref.csv")
 
 ##
-λs = readdlm("./application/artificial data/Lorenz96/Results/1 dimensional input/trial $(trial)/Lyaps_Lo96_N_8.csv")
-
+λs = readdlm("./application/artificial data/Lorenz96/Lyapunov spectrum/Lyaps_Lo96_N_8_4.csv")
+λs = λs[1:2:end,:]
 pos_Lyap_idx = λs[:,1] .> 10^-3
 
 non_embedding_idx = findall(optimal_d_pec.==1)
@@ -199,6 +201,175 @@ for RQA_val = 1:2:14
     ylims4 = axis4.get_ylim()
     vlines(Fs[pos_Lyap_idx], ylims4[1], ylims4[2], linestyle="dashed", linewidth=l_width_vert)
     title("MCDTS")
+    ylabel(RQA_names[RQA_val+1])
+    grid()
+end
+
+
+for RQA_val = 1:2:14
+
+    figure(figsize=(20,10))
+    axis1 = subplot(421)
+    plot(Fs, λs)
+    ylims1 = axis1.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims1[1], ylims1[2], linestyle="dashed", linewidth=l_width_vert)
+    title("Lyaps")
+    grid()
+
+    axis1 = subplot(422)
+    plot(Fs, λs)
+    ylims1 = axis1.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims1[1], ylims1[2], linestyle="dashed", linewidth=l_width_vert)
+    title("Lyaps")
+    grid()
+
+    axis2 = subplot(423)
+    plot(Fs, (abs.(RQA_tde[:,RQA_val] .- RQA_ref[:,RQA_val]) ./ RQA_ref[:,RQA_val]))
+    ylims2 = axis2.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims2[1], ylims2[2], linestyle="dashed", linewidth=l_width_vert)
+    title("TDE (rel. DIFFS)")
+    ylabel(RQA_names[RQA_val])
+    grid()
+
+    axis2 = subplot(424)
+    plot(Fs, (abs.(RQA_tde[:,RQA_val+1] .- RQA_ref[:,RQA_val+1]) ./ RQA_ref[:,RQA_val+1]))
+    ylims3 = axis2.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims3[1], ylims3[2], linestyle="dashed", linewidth=l_width_vert)
+    title("TDE (rel. DIFFS)")
+    ylabel(RQA_names[RQA_val+1])
+    grid()
+
+    axis3 = subplot(425)
+    var = (abs.(RQA_pec[:,RQA_val] .- RQA_ref[:,RQA_val]) ./ RQA_ref[:,RQA_val])
+    plot(Fs, var)
+    plot(Fs[n_e_i], var[n_e_i],"*")
+    axis3.set_ylim(ylims2)
+    #ylims3 = axis3.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims2[1], ylims2[2], linestyle="dashed", linewidth=l_width_vert)
+    title("PECUZAL (rel. DIFFS)")
+    ylabel(RQA_names[RQA_val])
+    grid()
+
+    axis3 = subplot(426)
+    var2 = (abs.(RQA_pec[:,RQA_val+1] .- RQA_ref[:,RQA_val+1]) ./ RQA_ref[:,RQA_val+1])
+    plot(Fs, var2)
+    plot(Fs[n_e_i], var2[n_e_i],"*")
+    axis3.set_ylim(ylims3)
+    #ylims3 = axis3.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims3[1], ylims3[2], linestyle="dashed", linewidth=l_width_vert)
+    title("PECUZAL (rel. DIFFS)")
+    ylabel(RQA_names[RQA_val+1])
+    grid()
+
+    axis4 = subplot(427)
+    plot(Fs, (abs.(RQA_mcdts[:,RQA_val] .- RQA_ref[:,RQA_val]) ./ RQA_ref[:,RQA_val]))
+    axis4.set_ylim(ylims2)
+    #ylims4 = axis4.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims2[1], ylims2[2], linestyle="dashed", linewidth=l_width_vert)
+    title("MCDTS (rel. DIFFS)")
+    ylabel(RQA_names[RQA_val])
+    grid()
+
+    axis4 = subplot(428)
+    plot(Fs, (abs.(RQA_mcdts[:,RQA_val+1] .- RQA_ref[:,RQA_val+1]) ./ RQA_ref[:,RQA_val+1]))
+    axis4.set_ylim(ylims3)
+    #ylims4 = axis4.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims3[1], ylims3[2], linestyle="dashed", linewidth=l_width_vert)
+    title("MCDTS (rel. DIFFS)")
+    ylabel(RQA_names[RQA_val+1])
+    grid()
+end
+
+for RQA_val = 1:2:14
+
+    figure(figsize=(20,10))
+    axis1 = subplot(421)
+    plot(Fs, λs)
+    ylims1 = axis1.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims1[1], ylims1[2], linestyle="dashed", linewidth=l_width_vert)
+    title("Lyaps")
+    grid()
+
+    axis1 = subplot(422)
+    plot(Fs, λs)
+    ylims1 = axis1.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims1[1], ylims1[2], linestyle="dashed", linewidth=l_width_vert)
+    title("Lyaps")
+    grid()
+
+    axis2 = subplot(423)
+    plot(Fs, RQA_ref[:,RQA_val], label = "ref")
+    plot(Fs, RQA_tde[:,RQA_val], label = "exp")
+    plot(Fs, RQA_tde[:,RQA_val] .- RQA_ref[:,RQA_val], color = "red",label = "diff")
+    ylims2 = axis2.get_ylim()
+    legend()
+    vlines(Fs[pos_Lyap_idx], ylims2[1], ylims2[2], linestyle="dashed", linewidth=l_width_vert)
+    title("TDE (abs. DIFFS)")
+    ylabel(RQA_names[RQA_val])
+    grid()
+
+    axis2 = subplot(424)
+    plot(Fs, RQA_ref[:,RQA_val+1], label = "ref")
+    plot(Fs, RQA_tde[:,RQA_val+1], label = "exp")
+    plot(Fs, RQA_tde[:,RQA_val+1] .- RQA_ref[:,RQA_val+1], color = "red",label = "diff")
+    ylims3 = axis2.get_ylim()
+    legend()
+    vlines(Fs[pos_Lyap_idx], ylims3[1], ylims3[2], linestyle="dashed", linewidth=l_width_vert)
+    title("TDE (abs. DIFFS)")
+    ylabel(RQA_names[RQA_val+1])
+    grid()
+
+    axis3 = subplot(425)
+    var = RQA_pec[:,RQA_val] .- RQA_ref[:,RQA_val]
+    plot(Fs, RQA_ref[:,RQA_val], label = "ref")
+    plot(Fs, RQA_pec[:,RQA_val], label = "exp")
+    plot(Fs, var, color = "red",label = "diff")
+    plot(Fs[n_e_i], var[n_e_i],"*")
+    legend()
+    axis3.set_ylim(ylims2)
+    #ylims3 = axis3.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims2[1], ylims2[2], linestyle="dashed", linewidth=l_width_vert)
+    title("PECUZAL (abs. DIFFS)")
+    ylabel(RQA_names[RQA_val])
+    grid()
+
+    axis3 = subplot(426)
+    var2 = RQA_pec[:,RQA_val+1] .- RQA_ref[:,RQA_val+1]
+    plot(Fs, RQA_ref[:,RQA_val+1], label = "ref")
+    plot(Fs, RQA_pec[:,RQA_val+1], label = "exp")
+    plot(Fs, var2, color = "red",label = "diff")
+    plot(Fs[n_e_i], var2[n_e_i],"*")
+    legend()
+    axis3.set_ylim(ylims3)
+    #ylims3 = axis3.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims3[1], ylims3[2], linestyle="dashed", linewidth=l_width_vert)
+    title("PECUZAL (abs. DIFFS)")
+    ylabel(RQA_names[RQA_val+1])
+    grid()
+
+    axis4 = subplot(427)
+    var = RQA_mcdts[:,RQA_val] .- RQA_ref[:,RQA_val]
+    plot(Fs, RQA_ref[:,RQA_val], label = "ref")
+    plot(Fs, RQA_mcdts[:,RQA_val], label = "exp")
+    plot(Fs, var, color = "red",label = "diff")
+    legend()
+    axis4.set_ylim(ylims2)
+    #ylims4 = axis4.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims2[1], ylims2[2], linestyle="dashed", linewidth=l_width_vert)
+    title("MCDTS (abs. DIFFS)")
+    ylabel(RQA_names[RQA_val])
+    grid()
+
+    axis4 = subplot(428)
+    var2 = RQA_mcdts[:,RQA_val+1] .- RQA_ref[:,RQA_val+1]
+    plot(Fs, RQA_ref[:,RQA_val+1], label = "ref")
+    plot(Fs, RQA_mcdts[:,RQA_val+1], label = "exp")
+    plot(Fs, var2, color = "red", label = "diff")
+    legend()
+    axis4.set_ylim(ylims3)
+    #ylims4 = axis4.get_ylim()
+    vlines(Fs[pos_Lyap_idx], ylims3[1], ylims3[2], linestyle="dashed", linewidth=l_width_vert)
+    title("MCDTS (abs. DIFFS)")
     ylabel(RQA_names[RQA_val+1])
     grid()
 end
