4d71975868bad94cf37d440b8327aec6b7dbf48d
K. Hauke Kraemer <hkraemer@pik-potsdam.de>
2021-09-06 10:12:59 +0200

    combustion: new results incorporated

--- application/Causality/Combustion/plot_results_CCM_combustion.m
+++ application/Causality/Combustion/plot_results_CCM_combustion.m
@@ -6,11 +6,11 @@
 
 clear, clc
          
-sample = 26;  % set 1: sample 1, set 2: sample 2 ... until sample 50.
+sample = 4;  % set 1: sample 1, set 2: sample 2 ... until sample 50.
 
 
 % load the results
-lstr1 = strcat('./results/results_analysis_CCM_full_combustion_',num2str(sample),'_');
+lstr1 = strcat('./results 4/results_analysis_CCM_full_combustion_',num2str(sample),'_');
 
 
 % set colors for bars
